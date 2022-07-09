75307441130d1c76cb410f7cc9a39455f2be6a79
William H <william.hermozo@protonmail.com>
2022-07-09 22:18:17 +0100

    chore: set up stitches

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
+import { styled } from "../stitches.config";
 
 const Home: NextPage = () => {
-  return <div>homepage hommyyy</div>;
+  return <Text>setting up stitches hommyyy</Text>;
 };
 
 export default Home;
+
+const Text = styled("p", {
+  fontSize: "36px",
+  fontWeight: "700",
+});
---
+++ styles/globalStyles.ts
@@ -0,0 +1,38 @@
+import { globalCss } from "../stitches.config";
+
+export const globalStyles = globalCss({
+  "*, *::before, *::after": {
+    boxSizing: "border-box",
+  },
+
+  "*": {
+    margin: 0,
+  },
+
+  "html, body": {
+    height: "100%",
+  },
+
+  body: {
+    lineHeight: 1.5,
+    webkitFontSmoothing: "antialiased",
+    "-webkit-font-smoothing": "antialiased",
+  },
+
+  "img, picture, video, canvas, svg": {
+    display: "block",
+    maxWidth: "100%",
+  },
+
+  "input, button, textarea, select": {
+    font: "inherit",
+  },
+
+  "p, h1, h2, h3, h4, h5, h6": {
+    overflowWrap: "break-word",
+  },
+
+  "#root, #__next": {
+    isolation: "isolate",
+  },
+});
