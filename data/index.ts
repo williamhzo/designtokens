b27772080b2a823dad3be933028bb978ac128d05
William H <william.hermozo@protonmail.com>
2022-07-10 15:16:22 +0100

    chore: split tokens in separate files

---
+++ data/index.ts
@@ -0,0 +1,9 @@
+import { screens } from "./tokens/screens";
+import { colors } from "./tokens/colors";
+import { spacing } from "./tokens/spacing";
+
+export const defaultTheme = {
+  screens,
+  colors,
+  spacing,
+};
---
+++ data/tokens/screens.ts
@@ -0,0 +1,9 @@
+export const screens = {
+  test: `
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
+    "2xl": "1536px",
+  `,
+};
