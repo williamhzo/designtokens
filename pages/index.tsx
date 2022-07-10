4b103d59248d6c60fb42f15d5e01065e29384fed
William H <william.hermozo@protonmail.com>
2022-07-10 15:16:54 +0100

    feat: trying stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,55 @@
 import type { NextPage } from "next";
-import { styled } from "../stitches.config";
+import { defaultTheme } from "../data/index";
 
 const Home: NextPage = () => {
-  return <Text>setting up stitches hommyyy</Text>;
+  const { colors, screens, spacing } = defaultTheme;
+
+  console.log("spacing", spacing);
+
+  return (
+    <div>
+      <h1>tailwind tokens!</h1>
+
+      <a
+        href="https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/defaultConfig.stub.js"
+        target="_blank"
+        rel="noreferrer"
+      >
+        tailwindcss default theme repository
+      </a>
+
+      {/* see https://ped.ro/writing/code-blocks-but-better !! */}
+      <pre>
+        <code>{screens.test}</code>
+      </pre>
+
+      <pre>
+        <code>{JSON.stringify(spacing)}</code>
+      </pre>
+
+      {/* {Object.entries(spacing).map(([key, value]) => (
+        <p key={value}>
+          {key} - {value}
+        </p>
+      ))}
+
+      {Object.entries(colors).map(([key, value]) => {
+        if (typeof value === "string") {
+          return (
+            <p key={value}>
+              {key} - {value}
+            </p>
+          );
+        } else {
+          return Object.entries(value).map(([nestedKey, nestedValue]) => (
+            <p key={nestedValue}>
+              {key} - {nestedKey} - {nestedValue}
+            </p>
+          ));
+        }
+       })} */}
+    </div>
+  );
 };
 
 export default Home;
-
-const Text = styled("p", {
-  fontSize: "36px",
-  fontWeight: "700",
-});
